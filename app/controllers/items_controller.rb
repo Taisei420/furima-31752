4d8a1196fa6c6c237cbf5f672a59dfec003c94c9
Taisei420 <taiseiblog96@gmail.com>
2020-11-29 23:07:09 +0900

    destroyアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]  
 
 
@@ -34,6 +34,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
+  end
+
 
 
 
